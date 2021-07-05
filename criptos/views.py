3457db578c7fc0bc57abe867e22f5b04a887defc
Daniel <torrcdaniel@gmail.com>
2021-07-05 20:18:53 +0200

    Finalizada ruta status y creación del readme

--- criptos/views.py
+++ criptos/views.py
@@ -13,9 +13,9 @@ url_api = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}
 
 def validaciones_criptos():
     try:
-        compras = consultaSQL("SELECT * FROM movimientos;")
+        compra_venta = consultaSQL("SELECT * FROM movimientos;")
         monedasdict = dict()
-        for i in compras:
+        for i in compra_venta:
             # Cuando compremos por 1ª vez el saldo será negativo. En el else las segundas y sucesivas compras las vamos acumulando con saldo negativo.
             if i['moneda_from'] not in monedasdict:
                 monedasdict[i['moneda_from']] = -i['cantidad_from']
@@ -29,7 +29,7 @@ def validaciones_criptos():
         return monedasdict
     except:
         flash("Se ha producido un error en la base de datos. Pruebe en unos minutos")
-        return render_template('compra.html')
+        return render_template("compra.html")
 
 
 @app.route('/')
@@ -66,9 +66,9 @@ def compra():
             resultado = api(formulario)
             if not isinstance(resultado, str):
                 precio = resultado['data']['quote'][formulario.para_moneda.data]['price']
-                precioprocesado = precio #formato numero
+                precioprocesado = precio 
                 formulario.c2.data = precioprocesado
-                punitario = formulario.c1.data/precio #formato numero
+                punitario = formulario.c1.data/precio 
                 formulario.c2.data = precioprocesado
                 return render_template("compra.html", form=formulario, cantidadconvertida=precioprocesado, preciounitario=punitario)
             else:
@@ -111,11 +111,11 @@ def compra():
 def status():
     saldosmonedas = validaciones_criptos()
     print("saldosmonedas:", saldosmonedas)
-
+    print("saldosmonedas:", saldosmonedas.items())
 
     # Consultamos en la base de datos para acceder a las monedas from que sean "EUR"    
     try:
-        from_dinero = consultaSQL('SELECT cantidad_from FROM movimientos WHERE moneda_from = "EUR"')
+        from_dinero = consultaSQL('SELECT cantidad_from FROM movimientos WHERE moneda_from = "EUR"')    
     except sqlite3.Error as el_error:
         print("Error en SQL", el_error)
         flash("Se ha producido un error en la base de datos. Pruebe en unos minutos", "error")
@@ -130,17 +130,36 @@ def status():
         saldos = 0
         return render_template("status.html", saldos=saldos)
 
-
-    for item, valor in saldosmonedas.items(): 
+    # INVERSIÓN ATRAPADA: Calculamos el valor actual de todas las criptos, quitándole los 'EUR' si los hubiera
+    if 'EUR' in saldosmonedas:
+        del saldosmonedas['EUR']
+    else:
+        pass
+    inv_atrapada=0
+    for item, valor in saldosmonedas.items():
         resultado = requests.get(url_api.format(valor, item, 'EUR', API_KEY))
         if resultado.status_code == 200:
             dict_precios = resultado.json()
-            # Procesamos los datos necesarios del diccionario de diccionarios que devuelve el json
-            precioactual = dict_precios['data']['quote']['EUR']['price']
+            print(dict_precios)
+            # Procesamos los datos necesarios del diccionario de diccionarios que devuelve el json hasta llegar al valor actual de la cripto
+            cotizacionactual = dict_precios['data']['quote']['EUR']['price']
+            inv_atrapada+=cotizacionactual
         if resultado.status_code != 200:
             flash("Error en API_KEY")
-            return render_template("status.html")
+            return render_template("status.html", saldos=0)
     
-
-    precioactual= cantidadinvertida + totaleuros + cotizacion
+    euros_from=0
+    for i in from_dinero:
+        euros_from += i['cantidad_from']
+    
+    euros_to=0
+    for i in to_dinero:
+        euros_to += i['cantidad_to']
+    
+    saldos_euros_invertidos = euros_to - euros_from
+    cantidadinvertida = round(euros_from, 2)
+    precioactual = round(cantidadinvertida + saldos_euros_invertidos + inv_atrapada, 2)
     diferencia = precioactual - cantidadinvertida
+    return render_template("status.html", cantidadinvertida=cantidadinvertida, precioactual=precioactual, saldos=diferencia)   
+
+    
--- readme.md
+++ readme.md
@@ -1,1 +1,71 @@
-## Crear las instrucciones y presentación del proyecto
+# :chart_with_downwards_trend:  Cripto-Conversor  :chart_with_upwards_trend:
+
+### Proyecto desarrollado con Flask | Bootcamp Zero de KeepCoding  :computer:
+___
+Aplicación que simula la compraventa de las siguintes 12 criptomonedas: 
+
+
+1.Bitcoin 'BTC'
+
+2.Ethereum 'ETH'
+
+3.Teher 'USDT'
+
+4.Binance 'BNB'
+
+5.Cardano 'ADA'
+
+6.XRP 'XRP'
+
+7.Bitcoin Cash 'BCH'
+
+8.Stellar 'XLM'
+
+9.Tron 'TRX'
+
+10.EOS 'EOS'
+
+11.Bitcoin SV 'BSV'
+
+12.Litecoin 'LTC'
+
+Se dispondrá de un saldo infinito en **euros €** para comprar. Las criptomonedas pueden intercambiarse entre ellas con el objetivo de hacer crecer nuestras inversiones. Para recuperar las inversiones de cualquier cripto, deberán intercambiarse por **euros €**.
+
+Instalación :mag_right:
+___
+* **Creación del entorno virtual en Windows (opcional)**
+
+**python -m venv venv**
+
+* **Creación del entorno virtual en Mac y Linux (opcional)**
+
+**python3 -m venv venv**
+
+* **Instalación del fichero requirements**
+
+**pip install -r requirements.txt**
+
+
+* **Fichero .env_template**
+
+Deberá duplicarse este fichero y renombrarlo a **.env**. Será elección del instalador si quiere trabajar en modo development o production.
+
+* **Fichero config_template.py**
+
+Deberá duplicarse este fichero y renombrarlo a **config.py** en él tendrás que realizar estos 3 pasos:
+
+1. Deberás establecer una clave secreta. Un buen sitio para encontrarla es randomkeygen.com
+2. Necesitarás una clave para la API. Visita coinmarketcap.com/api/ para obtenerla
+3. Establecer la ruta a tu base de datos.
+
+
+* **Creación de la base de datos**
+
+Informar de la ruta a la base de datos **data/movimientos.db** y ejecutar el fichero initial.sql dentro de la carpeta migrations. Se puede realizar con sqlite3 o con un cliente gráfico.
+
+* **Iniciar la aplicación**
+
+Ejecutar el comando: 
+> flask run
+
+
